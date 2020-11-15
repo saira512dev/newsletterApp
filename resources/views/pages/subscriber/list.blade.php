b0449933eb0fa92d609d9b863919b1faea79aa1c
saira <saira@ceribs.com>
2020-11-15 12:54:16 +0530

    old newsletters of all users can now be viewed

--- resources/views/pages/subscriber/list.blade.php
+++ resources/views/pages/subscriber/list.blade.php
@@ -1,1 +1,25 @@
 @extends('layouts.layout')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6 offset-lg-3 ">
+        
+            <h3 style="text-align:center">All Newsletters</h3>
+            <div class="table-responsive">
+                <table class="table table-striped table-hover mx-auto w-auto" >
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col" class="col-sm-3">Description</th>
+                    </tr>
+                    @foreach($newsletters as $newsletter)
+                        <tr>
+                            <td>{{$newsletter->title}}</td>
+                            <td>{{$newsletter->description}}</td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
